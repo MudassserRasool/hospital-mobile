4f6fd6ac8e732a586ae3f44a183beb24fe177f9b
Mudasser Rasool <86182941+MudassserRasool@users.noreply.github.com>
2025-11-16 16:52:35 +0500

    Refactor useThemeColor hook to always use light mode: Removed dynamic theme handling and commented out code for dark mode support in use-theme-color.ts, simplifying the color selection logic.

--- hooks/use-theme-color.ts
+++ hooks/use-theme-color.ts
@@ -3,14 +3,31 @@
  * https://docs.expo.dev/guides/color-schemes/
  */
 
+// uncomment this to use light and dark mode
+// import { Colors } from '@/constants/theme';
+// import { useColorScheme } from '@/hooks/use-color-scheme';
+
+// export function useThemeColor(
+//   props: { light?: string; dark?: string },
+//   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+// ) {
+//   const theme = useColorScheme() ?? 'light';
+//   const colorFromProps = props[theme];
+
+//   if (colorFromProps) {
+//     return colorFromProps;
+//   } else {
+//     return Colors[theme][colorName];
+//   }
+// }
+
 import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-  const theme = useColorScheme() ?? 'light';
+  const theme = 'light'; // Always use light mode
   const colorFromProps = props[theme];
 
   if (colorFromProps) {
