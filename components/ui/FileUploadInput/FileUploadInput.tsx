c93c219b7ef21187a9a6f8a6204d06ddd5d8a90a
Mudasser Rasool <86182941+MudassserRasool@users.noreply.github.com>
2025-11-27 09:04:35 +0500

    .

---
+++ components/ui/FileUploadInput/FileUploadInput.tsx
@@ -0,0 +1,14 @@
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import React from 'react';
+import styles from './FileUploadInput.style';
+
+const FileUploadInput = () => {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText>FileUploadInput</ThemedText>
+    </ThemedView>
+  )
+}
+
+export default FileUploadInput
